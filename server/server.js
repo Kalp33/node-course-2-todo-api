acc270348142ba05d79db5fdea6ac2355e5f5a36
Kalpesh Benare <GS-1903@GSLAB.COM>
2018-08-21 13:21:36 +0530

    POST request

--- server/server.js
+++ server/server.js
@@ -1,41 +1,31 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-var newTodo = new Todo({
-    text: 'Cook dinner'
+
+
+app.listen(3040, () => {
+   console.log('Started on port 3040');
 });
 
-var newTodo2 = new Todo({
-    text: 'Read Book',
-    completed: true,
-    completedAt: 1234
-});
-
-/*newTodo.save().then((doc) => {
-    console.log('Saved Todo:', doc);
-}, (e) => {
-    console.log('Unable to save Todo');
-});*/
-
-newTodo2.save().then((doc) => {
-    console.log('Saved Todo:', doc);
-}, (e) => {
-    console.log('Unable to save Todo');
-});
 
 
 

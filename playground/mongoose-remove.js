8eb82f1d231be3aa6eb732a2b9daeb9a1501295e
Kalpesh Benare <GS-1903@GSLAB.COM>
2018-08-24 12:14:21 +0530

    Delete todo by id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,12 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+/*Todo.remove({}).then((result) => {
+    console.log(result);
+});*/
+
+Todo.findByIdAndRemove('5b7fa596a4b5c616d809393a').then((todo) => {
+    console.log(todo);
+});

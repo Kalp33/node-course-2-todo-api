321c944f232df0e280bc2477751579d93de185c0
Kalpesh Benare <GS-1903@GSLAB.COM>
2018-08-29 12:00:40 +0530

    encrypting the password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,22 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+       console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$KMjai6P/YYEOOKuBuU7N9.bPUgobTiEUe.pskMsGEI4vY1/8FdVGK';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+/*var data = {
     id: 10
 }
 
@@ -10,6 +25,9 @@ console.log('token', token);
 
 var decoded = jwt.verify(token, '123abc');
 console.log('Decoded data: ', decoded);
+
+
+*/
 
 /* var message = 'I am user number 3';
 var hash = SHA256(message).toString();
